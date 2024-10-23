00dce2577d24ad743a181b24593caa4761ac28ae
carohansen73 <laaflaka.8@hotmail.com>
2024-10-23 15:06:23 -0300

    comienzo proyecto TP5 microservicio producto

---
+++ src/main/java/com/example/demo/ArqWebTp5ProductoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqWebTp5ProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqWebTp5ProductoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/model/Producto.java
@@ -0,0 +1,61 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Producto {
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Integer id;
+	
+	@Column    
+	private String nombre;
+	
+	@Column    
+	private double precio;
+	
+	public Producto() {
+		super();
+	}
+	
+	public Producto(String producto, double precio) {
+		this.nombre = producto;
+		this.precio = precio;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + "]";
+	}
+	
+	
+}
